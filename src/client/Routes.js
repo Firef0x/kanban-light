f3369f9ea5e54f945255876993da1077bb284a63
Firef0x <Firefgx@gmail.com>
2019-11-23 23:03:17 +0800

    refactor(App): Rename App.js
    Signed-off-by: Firef0x <Firefgx@gmail.com>

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import KanbanLightContainer from './containers/KanbanLightContainer/KanbanLightContainer';
-import KanbanLight from './components/KanbanLight/KanbanLight';
 
 /* Import Less styles */
-import './app.less';
+import './App.less';
 
 const Routes = (
   <BrowserRouter>
     <div id="app">
-      <Route component={KanbanLightContainer}>
-        <Route path="/" component={KanbanLight}>
-        </Route>
-      </Route>
+      <Route component={KanbanLightContainer} />
     </div>
   </BrowserRouter>
 );
